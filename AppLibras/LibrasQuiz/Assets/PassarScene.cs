145c0603bfe9d6d2e3587b69bf783841b5f9d563
eduhlira4 <ejlas.lira@gmail.com>
2017-12-01 08:53:23 -0400

    FirstCommit

---
+++ AppLibras/LibrasQuiz/Assets/PassarScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassarScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PassaCena (string cena){
+		SceneManager.LoadScene (cena);
+	}
+
+	public void FaseParametro(int faseLetraSilaba){
+		PlayerPrefs.SetInt("letraSilaba", faseLetraSilaba);
+		//SceneManager.LoadScene (PlayerPrefs.GetInt ("letraSilaba"));
+	}
+}
